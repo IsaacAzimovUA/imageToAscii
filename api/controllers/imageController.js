c1582f21a3ed0025f3de21a71125021c90c1a964
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-03-21 20:22:56 +0200

    add options for image to ascii serves

--- api/controllers/imageController.js
+++ api/controllers/imageController.js
@@ -2,8 +2,11 @@ const { convertToAscii } = require('../services/imageService');
 const path = require('path');
 
 const convertToImage = async (req, res) => {
-  const imageFile = path.resolve(__dirname, req.files.imageFile.tempFilePath);
-  const convertedImage = await convertToAscii(imageFile);
+  const IMAGE_FILE = path.resolve(__dirname, req.files.imageFile.tempFilePath);
+  const OPTIONS = req.body;
+  console.log('convertToImage ~ OPTIONS:', OPTIONS);
+
+  const convertedImage = await convertToAscii(IMAGE_FILE, OPTIONS);
   res.status(200).json({ payload: convertedImage });
 };
 
--- api/services/imageService.js
+++ api/services/imageService.js
@@ -1,14 +1,14 @@
 const asciifyImage = require('asciify-image');
 
-const convertToAscii = async (imageFile) => {
-  const options = {
+const convertToAscii = async (imageFile, options) => {
+  const DEFAULT_OPTIONS = {
     fit: 'box',
-    width: 200,
-    height: 100,
+    width: 100,
+    height: 50,
   };
 
   return new Promise((resolve, rejected) => {
-    asciifyImage(imageFile, options, (error, asciified) => {
+    asciifyImage(imageFile, options && DEFAULT_OPTIONS, (error, asciified) => {
       if (error) {
         rejected(error);
       } else {
