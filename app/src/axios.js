2665e03ad96c2adf059bd8f70570a886a1a745b5
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-03-28 10:56:52 +0200

    change docker files add compose.prod file

--- app/src/axios.js
+++ app/src/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const { API_PORT, API_HOST } = process.env;
-
 const axiosInstance = axios.create({
-  baseURL: `http://${API_HOST}:${API_PORT}`,
+  baseURL: `http://localhost:${process.env.API_PORT}`,
 });
 export default axiosInstance;
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const { APP_HOST, APP_PORT } = process.env;
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: APP_HOST,
-    port: APP_PORT,
+    host: true,
+    port: process.env.APP_PORT,
   },
   define: {
     'process.env': process.env,
